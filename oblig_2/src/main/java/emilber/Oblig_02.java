817b5322ba3c45f3adff1ea038ca3a407005cb1d
Emil Berglund <emil.berglund@live.no>
2025-01-22 16:00:51 +0100

    Finished 3/4

--- oblig_2/src/main/java/emilber/Oblig_02.java
+++ oblig_2/src/main/java/emilber/Oblig_02.java
@@ -5,41 +5,43 @@ import java.util.Scanner;
 public class Oblig_02 {
 
     private static void superlinear(long n) {
-        int tmp = 1;
         for (long i = 0; i < n; i++) {
-            for (long j = 0; j < n; j++) {
-                tmp *= 1;
+            for (long j = 1; j < n; j *= 2) {
+                // Do nothing
             }
         }
     }
 
     private static void cubical(long n) {
-        int tmp = 1;
         for (long i = 0; i < n; i++) {
             for (long j = 0; j < n; j++) {
                 for (long k = 0; k < n; k++) {
-                    tmp *= 1;
+                    // Do nothing
                 }
             }
         }
     }
 
     private static void exponential(long n) {
-        int tmp = 1;
         long squared = (long) Math.pow(2, n);
         for (long i = 0; i < squared; i++) {
-            tmp *= 1;
+            // Do nothing
         }
     }
     private static void combinatorical(long n) {
-        int tmp = 1;
-        
+        long nFaculty = 1;
+        for (long i = 1; i <= n; i++) {
+            nFaculty *= i;
+        }
+        for (long i = 0; i < nFaculty; i++) {
+            // Do nothing
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Skriv inn tall for algoritmen du vil teste: ");
-        System.out.println("1: Superlineær algoritme \n2: Kubisk algoritme \n3: Eksponentiell algoritme");
+        System.out.println("1: Superlineær algoritme \n2: Kubisk algoritme \n3: Eksponentiell algoritme \n4: Kombinatorisk algoritme");
         int choice = scanner.nextInt();
         System.out.println("Skriv inn tall for n: ");
         long n = scanner.nextLong();
@@ -53,7 +55,10 @@ public class Oblig_02 {
         } 
         else if (choice == 3) {
             exponential(n);
-        } 
+        }
+        else if (choice == 4) {
+            combinatorical(n);
+        }
         else {
             System.out.println("Ugyldig valg");
         }
@@ -61,6 +66,8 @@ public class Oblig_02 {
         System.out.println("Tid brukt: " + (endTime - startTime) + "ms");
         scanner.close();
     }
-    // Superlineær algoritme: 125_000 = 10 sekunder
+    // Superlineær algoritme 500_050_000 = 10 sekunder
     // Kubisk 2500 = 10 sekunder
+    // Eksponentiell --
+    // Kombinatorisk 15_500_000_000 (15 milliarder) = 10 sekunder
 }
