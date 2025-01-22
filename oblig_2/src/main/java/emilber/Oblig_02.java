11def6803adae3cacc59c651503cd7ec8a23de4a
Emil Berglund <emil.berglund@live.no>
2025-01-22 15:06:12 +0100

    Started on oblig2 - finished 2/4

--- oblig_2/src/main/java/emilber/Oblig_02.java
+++ oblig_2/src/main/java/emilber/Oblig_02.java
@@ -1,8 +1,66 @@
 package emilber;
 
+import java.util.Scanner;
+
 public class Oblig_02 {
-    
-    public static void superlinear(long n){
-        // Kode for superlineær algoritme
+
+    private static void superlinear(long n) {
+        int tmp = 1;
+        for (long i = 0; i < n; i++) {
+            for (long j = 0; j < n; j++) {
+                tmp *= 1;
+            }
+        }
     }
+
+    private static void cubical(long n) {
+        int tmp = 1;
+        for (long i = 0; i < n; i++) {
+            for (long j = 0; j < n; j++) {
+                for (long k = 0; k < n; k++) {
+                    tmp *= 1;
+                }
+            }
+        }
+    }
+
+    private static void exponential(long n) {
+        int tmp = 1;
+        long squared = (long) Math.pow(2, n);
+        for (long i = 0; i < squared; i++) {
+            tmp *= 1;
+        }
+    }
+    private static void combinatorical(long n) {
+        int tmp = 1;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Skriv inn tall for algoritmen du vil teste: ");
+        System.out.println("1: Superlineær algoritme \n2: Kubisk algoritme \n3: Eksponentiell algoritme");
+        int choice = scanner.nextInt();
+        System.out.println("Skriv inn tall for n: ");
+        long n = scanner.nextLong();
+        long startTime = System.currentTimeMillis();
+
+        if (choice == 1) {
+            superlinear(n);
+        } 
+        else if (choice == 2) {
+            cubical(n);
+        } 
+        else if (choice == 3) {
+            exponential(n);
+        } 
+        else {
+            System.out.println("Ugyldig valg");
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Tid brukt: " + (endTime - startTime) + "ms");
+        scanner.close();
+    }
+    // Superlineær algoritme: 125_000 = 10 sekunder
+    // Kubisk 2500 = 10 sekunder
 }
