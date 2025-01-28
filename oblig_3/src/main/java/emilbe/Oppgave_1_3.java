39873b5859326db7bef09fddc0fa7765d624b964
Emil Berglund <emil.berglund@live.no>
2025-01-28 15:50:29 +0100

    Created oblig3

---
+++ oblig_3/src/main/java/emilbe/Oppgave_1_3.java
@@ -0,0 +1,16 @@
+package emilbe;
+
+public class Oppgave_1_3 {
+    public static long C_rekursiv(int a, int m) {
+        
+
+
+        return none;
+    } 
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
