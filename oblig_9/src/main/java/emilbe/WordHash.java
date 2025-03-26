ac52d39040e88d3b320af6128eec54bb6e2415e8
Emil Berglund <emil.berglund@live.no>
2025-03-26 16:16:01 +0100

    Finished oblig9

--- oblig_9/src/main/java/emilbe/WordHash.java
+++ oblig_9/src/main/java/emilbe/WordHash.java
@@ -1,52 +1,80 @@
 package emilbe;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
-/**
- * @author Emil Berglund
- */
+// WordHash: HashMap for lagring av ord og ordfrekvenser
 public class WordHash {
+    private HashMap<String, Integer> wordMap;
 
-    public static void main(String[] args) {
-        // Oppretter en tom hashtabell H med String som key og Integer
-        // som dataverdier. Tabellen får default lengde og
-        // default maks. lastfaktor.
-        HashMap<String, Integer> H = new HashMap<String, Integer>();
+    // WordHash(): Konstruktør som lager en tom hashtabell
+    public WordHash() {
+        wordMap = new HashMap<>();
+    }
 
-        // Legger inn syv ord med tilhørende tallverdier
-        H.put("pear", 4);
-        H.put("banana", 9);
-        H.put("apple", 8);
-        H.put("coconut", 2);
-        H.put("mango", 10);
-        H.put("peach", 5);
-        H.put("papaya", 5);
+    // size(): Antall ord som er lagret i hashtabellen
+    public int size() {
+        return wordMap.size();
+    }
 
-        // Les et ord fra bruker
-        Scanner scan = new Scanner(System.in);
-        System.out.print("? ");
-        String ord = scan.next();
-        scan.close();
+    // insert(): Setter inn ny forekomst av et ord
+    public void insert(String ord) {
+        wordMap.put(ord, wordMap.getOrDefault(ord, 0) + 1);
+    }
 
-        // Skriv ut ordet hvis det finnes i hashtabellen
-        if (H.containsKey(ord)) {
-            System.out.println(ord + ": " + H.get(ord));
-        }
-
-        // Oppdaterer en verdi i tabellen
-        H.replace("coconut", 3);
-
-        // Fjerner en verdi fra tabellen
-        H.remove("mango");
-
-        // Skriver ut antall data, og alle verdiene i
-        // tabellen ved å iterere over nøkkelverdiene
-        System.out.println("\nAll " + H.size()
-                + " elements in hash table:\n");
-        for (String key : H.keySet()) {
-            System.out.println(key + ": " + H.get(key));
+    // search(): Søk etter et ord. Skriv ut ordet og ordfrekvensen hvis det finnes i hashtabellen.
+    public void search(String ord) {
+        if (wordMap.containsKey(ord)) {
+            System.out.println(ord + ": " + wordMap.get(ord));
+        } else {
+            System.out.println("Word not found");
         }
     }
 
+    // print(): Skriv ut alle ord og ordfrekvenser i hashtabellen i alfabetisk sortert rekkefølge
+    public void print() {
+        TreeMap<String, Integer> sortedMap = new TreeMap<>(wordMap);
+        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    // main(): Testprogram
+    public static void main(String[] args) {
+        // Leser filnavn fra bruker
+        Scanner scan = new Scanner(System.in);
+        System.out.print("File? ");
+        String fileName = scan.next();
+
+        // Oppretter ordleser og tom hashtabell
+        WordReader wR = new WordReader(fileName);
+        WordHash wHash = new WordHash();
+
+        // Leser alle ordene på filen og legger inn i hashtabellen
+        String ord = wR.nextWord();
+        while (ord != null) {
+            wHash.insert(ord);
+            ord = wR.nextWord();
+        }
+        // Skriver ut antall ulike ord som fantes i filen
+        System.out.println(wHash.size() + " unique words " +
+                "read from file " + fileName);
+
+        // Menyvalg for å teste programmet
+        int valg = 0;
+        while (valg != 3) {
+            System.out.print("\n1:Search, 2:Print, 3:Quit ? ");
+            valg = scan.nextInt();
+            if (valg == 1) {
+                System.out.print("Search for? ");
+                ord = scan.next();
+                wHash.search(ord.toLowerCase());
+            } else if (valg == 2) {
+                wHash.print();
+            }
+        }
+        scan.close();
+    }
 }
